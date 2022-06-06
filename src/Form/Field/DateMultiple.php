7d87673c8fd5fb81bffb7de86fbe31adbd7ec113
Torow <736250253@qq.com>
2022-06-06 16:33:07 +0800

    Update DateMultiple.php

--- src/Form/Field/DateMultiple.php
+++ src/Form/Field/DateMultiple.php
@@ -5,15 +5,15 @@ namespace Encore\Admin\Form\Field;
 class DateMultiple extends Text
 {
     protected static $css = [
-        'https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css',
-        'https://cdn.jsdelivr.net/npm/shortcut-buttons-flatpickr@0.3.0/dist/themes/light.min.css',
+        //'https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css',
+        //'https://cdn.jsdelivr.net/npm/shortcut-buttons-flatpickr@0.3.0/dist/themes/light.min.css',
 
     ];
 
     protected static $js = [
-        'https://cdn.jsdelivr.net/npm/flatpickr',
-        'https://cdn.jsdelivr.net/npm/shortcut-buttons-flatpickr@0.1.0/dist/shortcut-buttons-flatpickr.min.js',
-        'https://npmcdn.com/flatpickr@4.6.6/dist/l10n/zh.js',
+        //'https://cdn.jsdelivr.net/npm/flatpickr',
+        //'https://cdn.jsdelivr.net/npm/shortcut-buttons-flatpickr@0.1.0/dist/shortcut-buttons-flatpickr.min.js',
+        //'https://npmcdn.com/flatpickr@4.6.6/dist/l10n/zh.js',
 
     ];
 
